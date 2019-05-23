fb2f794216d419ae204dc04aaf0b4700197db74c
Paulo Victor <paulovictorce@hotmail.com>
2019-05-23 18:09:08 -0300

    Criação da SPA e dos componentes que vão compor o template da aplicação, como o cabeçalho, o menu lateral e o rodapé.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='' target='_blank'> LPI - Soluções e Sistemas</a>.
+        </strong>
+    </footer>
+)
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
     entry: './src/index.jsx',
     output: {
@@ -14,11 +15,11 @@ module.exports = {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
-            jquery: 'modules/admin-lte/plugins/jQuery/jquey-2.2.3.min.js',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
