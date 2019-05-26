61d1f20bdad2111e9dbc6732613e14ac73f4b742
Paulo Victor <paulovictorce@hotmail.com>
2019-05-26 10:15:32 -0300

    Ligando o componente Dashboard ao Redux. Criação da ação getSummary.

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { getSummary } from '../dashboard/dashboardActions'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+    
+    componentWillMount() {
+        this.props.getSummary()
+    }
+    
     render() {
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
-                        <ValueBox cols='12 4' color='green' icon='bank' text='Total de Créditos' value='R$ 10,00' />
-                        <ValueBox cols='12 4' color='red' icon='credit-card' text='Total de Débitos' value='R$ 10,00' />
-                        <ValueBox cols='12 4' color='blue' icon='money' text='Consolidado' value='R$ 0,00' />
+                        <ValueBox cols='12 4' color='green' icon='bank' text='Total de Créditos' value={`R$ ${credit}`} />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' text='Total de Débitos' value={`R$ ${debt}`} />
+                        <ValueBox cols='12 4' color='blue' icon='money' text='Consolidado' value={`R$ ${credit - debt}`} />
                     </Row>
                 </Content>
             </div>
@@ -22,4 +33,8 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
+
+const mapDispatchToProps = dispatch => bindActionCreators({ getSummary }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
