d4e75df9a7af92c50f274865d334980cae75d8f0
Paulo Victor <paulovictorce@hotmail.com>
2019-05-28 17:32:07 -0300

    Refatorando o componente BillingCycle para se tornar um componente de classe. Criação dos componentes que vão ser responsáveis por criar abas e seus conteúdos na sessão de Ciclo de Pagamentos.

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default props => (
-    <div>
-        <h1>BillingCycle</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+import Tabs from '../common/tab/tabs'
+import TabsContent from '../common/tab/tabsContent'
+import TabsHeader from '../common/tab/tabsHeader'
+
+class BillingCycle extends Component {
+   render() {
+       return (
+        <div>
+            <ContentHeader title = "Ciclos de Pagamento" small = "Cadastro" />
+            <Content>
+                <Tabs>
+                    <TabsHeader>
+
+                    </TabsHeader>
+                    <TabsContent>
+                        
+                    </TabsContent>
+                </Tabs>
+            </Content>
+        </div>
+       )
+   }
+}
+
+export default BillingCycle
+ 
